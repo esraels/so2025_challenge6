f775adb804ef51a8f50a7e44d6f7623c55f41b3c
acegs <skysangue@gmail.com>
2025-09-28 23:25:48 +0800

    Fixed build script. Prevent running the app when there is syntax errors.

--- cpp/build_run.sh
+++ cpp/build_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-g++ main.cpp -o out/app -std=c++17
-#g++ main.cpp -o out/app -std=c++17 -O2
-#g++ main.cpp -o out/app -std=c++17 -O3
+#g++ main.cpp -o out/app -std=c++17 || exit 1
+g++ main.cpp -o out/app -std=c++17 -O2 || exit 1
+#g++ main.cpp -o out/app -std=c++17 -O3 || exit 1
 
 
 ./out/app
