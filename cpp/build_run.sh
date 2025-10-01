63d2307206ee565973e41a5f94cffcd3c557e03c
acegs <skysangue@gmail.com>
2025-10-01 12:31:08 +0800

    Improved build scripts.

--- cpp/build_run.sh
+++ cpp/build_run.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-#g++ main.cpp -o out/app -std=c++17 || exit 1
-#g++ main.cpp -o out/app -std=c++17 -O2 || exit 1
-g++ main.cpp -o out/app -std=c++17 -O3 || exit 1
-
-
-
-#./out/app $*
-
-
-cd input
-../out/app $*  100_nums.txt 10k_nums.txt 1M_nums.txt
-cd ..
+./build.sh --std=c++17 -O3 || exit $?
+./run_app.sh $* || exit $?
